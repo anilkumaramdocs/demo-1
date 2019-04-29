776969fd12ea2973308aee635da97220f2891a7f
anilkum <anilkumar.amdocs@gmail.com>
2019-04-30 00:06:32 +0530

    commit and publush post createing new repp demo-1 in github

--- demo-1/src/main/java/com/example/demo/Demo1Application.java
+++ demo-1/src/main/java/com/example/demo/Demo1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo1Application {
 
-	//version first update  
-	public static void main(String[] args) {
+	//version first update   
+	public static void main(String[] args) { 
 		SpringApplication.run(Demo1Application.class, args);
 	}
 
